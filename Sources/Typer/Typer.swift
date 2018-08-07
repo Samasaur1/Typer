d20f17b4cf9a25bc47461cfcf6468a5834b2c2b7
Sam <samgauck@icloud.com>
2018-08-07 17:45:01 -0400

    Escape more characters

--- Sources/Typer/Typer.swift
+++ Sources/Typer/Typer.swift
@@ -6,11 +6,17 @@ public final class Typer {
     public static func type(_ text: String) {
         do {
             for character in text {
-                var toPrint: String = String(character)
+                var toPrint: String
                 if character == "'" {
                     toPrint = "\\'"
+                } else if character == "\n" || character == "\r" {
+                    toPrint = "return"
+                } else if character == "\t" {
+                    toPrint = "tab"
+                } else {
+                    toPrint = "\"\(character)\""
                 }
-                try shellOut(to: "osascript", arguments: ["-e", "'tell application \"System Events\" to keystroke \"\(toPrint)\"'"])
+                try shellOut(to: "osascript", arguments: ["-e", "'tell application \"System Events\" to keystroke \(toPrint)'"])
               //usleep(1000000) <- 1 second
                 usleep(0020000)
             }
