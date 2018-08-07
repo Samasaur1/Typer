2c7e47fec7fe39e268809fe861f0c002fc48444c
Sam <samgauck@icloud.com>
2018-08-07 16:40:48 -0400

    Attempt Typing

--- Sources/Typer/Typer.swift
+++ Sources/Typer/Typer.swift
@@ -1,3 +1,9 @@
-struct Typer {
-    var text = "Hello, World!"
+import Foundation
+import ShellOut
+
+public final class Typer {
+    private init() {}
+    public static func type(_ text: String) {
+        try? shellOut(to: "osascript", arguments: ["-e", "tell application \"System Events\" to keystroke \"\(text)\""])
+    }
 }
--- Tests/TyperTests/TyperTests.swift
+++ Tests/TyperTests/TyperTests.swift
@@ -2,15 +2,12 @@ import XCTest
 @testable import Typer
 
 final class TyperTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Typer().text, "Hello, World!")
+    func typeEmptyString() {
+        type("")
     }
 
 
     static var allTests = [
-        ("testExample", testExample),
+        ("typeEmptyString", typeEmptyString),
     ]
 }
