9d1a59caeea67a739855ab4b79eae00726f40be3
Sam <samgauck@icloud.com>
2018-08-07 17:37:36 -0400

    Escape all required characters

--- Sources/Typer/Typer.swift
+++ Sources/Typer/Typer.swift
@@ -6,11 +6,14 @@ public final class Typer {
     public static func type(_ text: String) {
         do {
             for character in text {
-                try shellOut(to: "osascript", arguments: ["-e", "'tell application \"System Events\" to keystroke \"\(character)\"'"])
+                var toPrint: String = String(character)
+                if character == "'" {
+                    toPrint = "\\'"
+                }
+                try shellOut(to: "osascript", arguments: ["-e", "'tell application \"System Events\" to keystroke \"\(toPrint)\"'"])
               //usleep(1000000) <- 1 second
                 usleep(0020000)
             }
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) { print("Success!'") }
         } catch let error as ShellOutError {
             print(error.message)
             print(error.output)
