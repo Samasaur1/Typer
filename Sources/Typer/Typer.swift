e0f40ca79e03b85d45367534b7151b65595c1f11
Sam <samgauck@icloud.com>
2018-08-07 16:48:18 -0400

    Add error handling

--- Sources/Typer/Typer.swift
+++ Sources/Typer/Typer.swift
@@ -4,6 +4,13 @@ import ShellOut
 public final class Typer {
     private init() {}
     public static func type(_ text: String) {
-        try? shellOut(to: "osascript", arguments: ["-e", "tell application \"System Events\" to keystroke \"\(text)\""])
+        do {
+            try shellOut(to: "osascript", arguments: ["-e", "tell application \"System Events\" to keystroke \"\(text)\""])
+        } catch let error as ShellOutError {
+            print(error.message)
+            print(error.output)
+        } catch {
+            fatalError("THis is really bad")
+        }
     }
 }
