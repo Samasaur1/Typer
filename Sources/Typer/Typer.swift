dcb66eb6897b15b1a31194509b3a70abf3e33b6c
Sam <samgauck@icloud.com>
2018-08-09 08:34:33 -0400

    Remove ShellOut depencency and fix " escaping
    Switched from ShellOut to using the built-in process and fixed escaping of quotation marks

--- Sources/Typer/Typer.swift
+++ Sources/Typer/Typer.swift
@@ -1,5 +1,4 @@
 import Foundation
-import ShellOut
 
 public final class Typer {
     private init() {}
@@ -7,57 +6,48 @@ public final class Typer {
         static var initialized = false
     #endif
     public static func type(_ text: String, typing: Rate = .natural, printAlongWith printing: Bool = false) {
-        do {
-            for character in text {
-                var toPrint: String
-                if character == "'" {
-                    toPrint = "\"'\"'\"'\""
-                } else if character == "\n" || character == "\r" {
-                    toPrint = "return"
-                } else if character == "\t" {
-                    toPrint = "tab"
-                } else if character == "\\" {
-                    toPrint = "\"\\\""
-                } else if character == "\"" {
-                    toPrint = "'\"'\\\"'\"'"
-                } else {
-                    toPrint = "\"\(character)\""
-                }
-//                try shellOut(to: "osascript", arguments: ["-e", "'tell application \"System Events\" to keystroke \(toPrint)'"])
-                let p = Process()
-                p.launchPath = "/usr/bin/osascript"
-                p.arguments = ["-e", "'tell application \"System Events\" to keystroke \(toPrint)'"]
-                p.launch()
-                p.waitUntilExit()
-                if printing { print(toPrint) }
-                switch typing {
-                case .allAtOnce:
-                    usleep(0001000)
-                case .consistent:
-                    usleep(0020000)
-                case .natural:
-                    #if os(Linux)
-                        if !initialized {
-                            srandom(UInt32(time(nil)))
-                            initialized = true
-                        }
-                        let rand = UInt32(random())
-                    #else
-                        let rand = arc4random()
-                    #endif
-                    var sleepTime = rand % 5
-                    sleepTime *= 15000
-                    usleep(0020000 + sleepTime)
-                }
-              //usleep(1000000) <- 1 second
-//                usleep(0020000)
+        for character in text {
+            var toPrint: String
+            if character == "'" {
+                toPrint = "\"'\"'\"'\""
+            } else if character == "\n" || character == "\r" {
+                toPrint = "return"
+            } else if character == "\t" {
+                toPrint = "tab"
+            } else if character == "\\" {
+                toPrint = "\"\\\""
+            } else if character == "\"" {
+                toPrint = "\"\\\"\""
+            } else {
+                toPrint = "\"\(character)\""
             }
-        } catch let error as ShellOutError {
-            print(error.message)
-            print(error.output)
-            exit(1)
-        } catch {
-            fatalError("This is really bad: Unknown error")
+            let p = Process()
+            p.launchPath = "/usr/bin/osascript"
+            p.arguments = ["-e", "'tell application \"System Events\" to keystroke \(toPrint)'"]
+            p.launch()
+            p.waitUntilExit()
+            if printing { print(toPrint) }
+            switch typing {
+            case .allAtOnce:
+                usleep(0001000)
+            case .consistent:
+                usleep(0020000)
+            case .natural:
+                #if os(Linux)
+                if !initialized {
+                    srandom(UInt32(time(nil)))
+                    initialized = true
+                }
+                let rand = UInt32(random())
+                #else
+                let rand = arc4random()
+                #endif
+                var sleepTime = rand % 5
+                sleepTime *= 15000
+                usleep(0020000 + sleepTime)
+            }
+            //usleep(1000000) <- 1 second
+//            usleep(0020000)
         }
     }
     public enum Rate {
