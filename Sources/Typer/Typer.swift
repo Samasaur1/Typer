1f51aeb67ec5fce4f669d343fb19adbb947ba233
Sam <samgauck@icloud.com>
2018-08-31 14:41:50 -0400

    Add documentation

--- Sources/Typer/Typer.swift
+++ Sources/Typer/Typer.swift
@@ -1,10 +1,16 @@
 import Foundation
 
+/// A class that allows Swift programs to type, simulating using the keyboard.
 public final class Typer {
+    /// A private initializer so this class can't be instantiated.
     private init() {}
-    #if os(Linux)
-        static var initialized = false
-    #endif
+    /// Types the given text as if the user was doing so on the keyboard.
+    ///
+    /// - Parameters:
+    ///   - text: The text to type.
+    ///   - typing: The rate to type at. Default: `.natural`.
+    ///   - printing: Whether the text should be printed to STDOUT as it is typed. Default: `false`.
+    ///   - debug: Whether this is typing in debug mode (prints extra information). Default: `false`.
     public static func type(_ text: String, typing: Rate = .natural, printAlongWith printing: Bool = false, debug: Bool = false) {
         for character in text {
             if debug {
@@ -72,6 +78,13 @@ public final class Typer {
 //            usleep(0020000)
         }
     }
+    /// An enum representing possible typing rates.
+    ///
+    /// - allAtOnce: All the text should be typed at once.
+    /// - consistent: The text should be typed at a reasonable speed, but with no variance in delay.
+    /// - natural: The text should be typed so that it appears natural.
+    /// - customConsistent: The text should be typed at a consistent speed, specified by the associated value.
+    /// - customVarying: The text should be typed around a given speed, with 5 possible ranges of variation. Both the base speed and the maximum variance are specified by associated values.
     public enum Rate {
         case allAtOnce
         case consistent
